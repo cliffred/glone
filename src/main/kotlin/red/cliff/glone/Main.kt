3f7c7386c01206e972154f17eb01bab0e1220d29
Cliffred van Velzen <cliffred@cliffred.nl>
2023-11-16 15:59:24 +0100

    Use same semaphores for all groups to limit parallel calls for the whole application instead of per group

--- src/main/kotlin/red/cliff/glone/Main.kt
+++ src/main/kotlin/red/cliff/glone/Main.kt
@@ -3,11 +3,18 @@ package red.cliff.glone
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Semaphore
 
-suspend fun main(projectNames: Array<String>) {
-    GitlabApi().use { api ->
+suspend fun main(groups: Array<String>) {
+    val httpCallsSemaphore = Semaphore(10)
+    val gitClonesSemaphore = Semaphore(10)
+
+    GitlabApi(
+        httpCallsSemaphore = httpCallsSemaphore,
+        gitClonesSemaphore = gitClonesSemaphore,
+    ).use { api ->
         coroutineScope {
-            projectNames.forEach { name ->
+            groups.forEach { name ->
                 launch {
                     val projects: Flow<Project> = api.getProjects(name)
                     api.cloneProjects(projects)
