1c8df8e93c66a067cb68a88989ac4877b486e9e9
Cliffred van Velzen <cliffred@cliffred.nl>
2023-12-27 20:24:19 +0100

    Use output console make it replaceable

--- src/main/kotlin/red/cliff/glone/output.kt
+++ src/main/kotlin/red/cliff/glone/output.kt
@@ -4,9 +4,35 @@ val lineSeparator: String = System.lineSeparator()
 
 val lineSeparators = Regex("\r?\n")
 
-fun echo(message: Any? = "", trailingNewline: Boolean = true, err: Boolean = false) {
+fun echo(
+    message: Any? = "",
+    trailingNewline: Boolean = true,
+    error: Boolean = false,
+    console: OutputConsole = OutputContext.outputConsole
+) {
     var output = message?.toString()?.replace(lineSeparators, lineSeparator) ?: "null"
     output = if (trailingNewline) "$output$lineSeparator" else output
 
-    (if (err) System.err else System.out).print(output)
+    console.print(output, error)
+}
+
+interface OutputConsole {
+    fun print(text: String, error: Boolean)
+}
+
+class SystemOutputConsole : OutputConsole {
+    override fun print(text: String, error: Boolean) {
+        (if (error) System.err else System.out).print(text)
+    }
+}
+
+object OutputContext {
+
+    private val outputConsoles: ThreadLocal<OutputConsole> = ThreadLocal.withInitial { SystemOutputConsole() }
+    val outputConsole: OutputConsole
+        get() = outputConsoles.get()
+
+    fun setOutputConsole(outputConsole: OutputConsole) {
+        outputConsoles.set(outputConsole)
+    }
 }
