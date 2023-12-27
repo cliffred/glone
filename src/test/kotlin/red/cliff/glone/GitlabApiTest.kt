750b77cf94b55cdd6e064479973fbb318a916025
Cliffred van Velzen <cliffred@cliffred.nl>
2023-12-28 00:16:36 +0100

    Add GitlabApi test

---
+++ src/test/kotlin/red/cliff/glone/GitlabApiTest.kt
@@ -0,0 +1,23 @@
+package red.cliff.glone
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import kotlinx.coroutines.flow.filterNot
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
+
+class GitlabApiTest :
+    ShouldSpec({
+        should("get projects").config(enabledIf = { System.getenv("GITLAB_TOKEN") != null }) {
+            val gitlabApi = GitlabApi()
+
+            gitlabApi
+                .getProjects("EDSN/car-alm")
+                .filterNot { it.archived || it.emptyRepo }
+                .onEach { println(it) }
+                .take(5)
+                .toList()
+                .shouldHaveSize(5)
+        }
+    })
