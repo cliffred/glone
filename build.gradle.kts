c33fece47c8b22323858da5801b630473362c54f
Cliffred van Velzen <cliffred@cliffred.nl>
2025-10-15 08:58:06 +0200

    Update dependencies and Gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,26 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "2.2.20"
     application
-    id("org.graalvm.buildtools.native") version "0.10.0"
+    id("org.graalvm.buildtools.native") version "0.11.1"
 }
 
 group = "red.cliff"
 version = "1.0-SNAPSHOT"
 
 dependencies {
-    val ktorVersion = "2.3.8"
-    val kotestVersion = "5.8.0"
+    val ktorVersion = "3.3.1"
+    val kotestVersion = "6.0.4"
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
-    runtimeOnly("ch.qos.logback:logback-classic:1.4.14")
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.19")
 
-    testImplementation("io.mockk:mockk:1.13.9")
+    testImplementation("io.mockk:mockk:1.14.6")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    testImplementation("ch.qos.logback:logback-classic:1.4.14")
+    testImplementation("ch.qos.logback:logback-classic:1.5.19")
 }
 
 tasks.test {
