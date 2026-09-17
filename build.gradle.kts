73b3b2fd253af6ee13de01f001a35eadb1246778
Cliffred van Velzen <cliffred@cliffred.nl>
2026-09-17 09:09:51 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.2.20"
+    kotlin("jvm") version "2.4.20"
     application
     id("org.graalvm.buildtools.native") version "1.1.13"
 }
@@ -8,19 +8,19 @@ group = "red.cliff"
 version = "1.0-SNAPSHOT"
 
 dependencies {
-    val ktorVersion = "3.3.1"
-    val kotestVersion = "6.0.4"
+    val ktorVersion = "3.6.0"
+    val kotestVersion = "6.2.5"
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
-    runtimeOnly("ch.qos.logback:logback-classic:1.5.19")
+    runtimeOnly("ch.qos.logback:logback-classic:1.6.3")
 
-    testImplementation("io.mockk:mockk:1.14.6")
+    testImplementation("io.mockk:mockk:1.14.11")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    testImplementation("ch.qos.logback:logback-classic:1.5.19")
+    testImplementation("ch.qos.logback:logback-classic:1.6.3")
 }
 
 tasks.test {
@@ -29,7 +29,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
 }
 
 tasks.named<JavaExec>("run") {
